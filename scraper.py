2db127964f9894d9a20d1feda6e533cf3e3b633c
Szymon Ścibiorski <147078927+SzymonScib@users.noreply.github.com>
2024-05-14 17:55:21 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,11 +5,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import webbrowser
 
-#Write your USER-AGENT header into this dictionary
-header = {
-    'User-Agent': 'Your user-agent'
-}
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
