32fde448445a4c9ad14241b529201bde8aadd53b
SzymonScib <szymonscibcs@gmail.com>
2024-05-17 13:41:23 +0200

    Basic slider and data filtering

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from scraper import scrape_historical_data, header
+from scraper import scrape_historical_data, headers
+import os
+import pandas as pd
 
-scrape_historical_data(header, 'AAPL', './data/')
+def filter_data(files):
+    for file in files:
+        df = pd.read_csv('./data/' + file)
+        df_reduced = df.iloc[750:]
+        df_reduced.to_csv('./data/' + file, index=False)
+
+def sliding_window(file):
+    df = pd.read_csv('./data/' + file)
+    num_records = df.shape[0]
+    means = []
+
+    for i in range(int(num_records/5)):
+        subset = df['Close'].iloc[i:i+5]
+        mean_price = subset.mean()
+        means.append(mean_price)
