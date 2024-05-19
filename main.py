453229ffe988e47232d13c6b6b4232bb56d53b0a
Szymon Ścibiorski <147078927+SzymonScib@users.noreply.github.com>
2024-05-19 19:35:38 +0200

    Updated data_windowing function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from scraper import scrape_historical_data, headers
-import os
 import pandas as pd
 
 def filter_data(files):
@@ -8,12 +7,16 @@ def filter_data(files):
         df_reduced = df.iloc[750:]
         df_reduced.to_csv('./data/' + file, index=False)
 
-def sliding_window(file):
+def data_windowing(file, window_size):
     df = pd.read_csv('./data/' + file)
     num_records = df.shape[0]
-    means = []
+    close_prices = df['Close'].values
+    input_sequence, output = [], []
+    for i in range(num_records - window_size):
+        input_sequence.append(close_prices[i:i + window_size])
+        output.append(close_prices[i + window_size])
+    
+    return input_sequence, output
 
-    for i in range(num_records-5):
-        subset = df['Close'].iloc[i:i+5]
-        mean_price = subset.mean()
-        means.append(mean_price)
+
+a, b  = data_windowing('AMD_historical_data.csv', 5)
